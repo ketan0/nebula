12426c17fd9c26ff0308ed87ba47ef3923864d80
Ketan Agrawal <agrawalk@stanford.edu>
2021-04-18 22:01:09 -0700

    dir local vars for org-roam, HTML export

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((org-roam-directory . ".")
+         (org-roam-db-location . "./org-roam.db")))
+ (org-mode . ((org-html-head . "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles.css\" />")
+              (eval . (ketan0/org-html-auto-export-mode)))))
