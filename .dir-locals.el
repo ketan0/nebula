63757a117976b20523fb9eb2d85a101e2f258ef2
Ketan Agrawal <agrawalk@stanford.edu>
2021-09-01 15:45:01 -0400

    new content

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,1 @@
-((nil . ((org-roam-directory . "~/garden-simple/org")
-         (org-roam-db-location . "~/garden-simple/org/org-roam.db")))
- (org-mode . ((ketan0/org-html-auto-export-mode . t))))
+((org-mode . ((ketan0/org-html-auto-export-mode . t))))
