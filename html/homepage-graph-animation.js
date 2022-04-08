37ff79d9f3bf284539b2db17edf8537ef304897c
Ketan Agrawal <agrawalk@stanford.edu>
2022-04-08 03:36:23 -0700

    made the animations resizable and cooler

--- html/homepage-graph-animation.js
+++ html/homepage-graph-animation.js
@@ -2,8 +2,8 @@
 async function homePageGraphAnimation() {
   // that way the <a>'s are positioned absolutely, relative to the parent
   // https://css-tricks.com/absolute-positioning-inside-relative-positioning/
-  d3.select('div[class^=\'outline-text\']')
-    .style('position', 'relative');
+  container = d3.select('div[class^=\'outline-text\']')
+                .style('position', 'relative');
   // select <a> nodes, get their x/y coords
   const selection = d3.selectAll('a:not([href^="http"],[href="/"])')
 
@@ -24,22 +24,26 @@ async function homePageGraphAnimation() {
   });
   const updateSelection = selection.data(nodes);
   const simulation = d3.forceSimulation(nodes)
-    .force('x', d3.forceX(400))
-    .force('y', d3.forceY(150).strength(0.2))
-    .force('collide', d3.forceCollide(20))
-    .force('charge', d3.forceManyBody().strength(-600))
+    .force('x', d3.forceX(50))
+    .force('y', d3.forceY(50).strength(0.2))
+    .force('collide', d3.forceCollide(10))
+    .force('charge', d3.forceManyBody().strength(-1))
     .on('tick', function () {
+      containerRect = container.node().getBoundingClientRect();
+      // console.log(`container width: ${containerRect.width} height: ${containerRect.height}`)
       updateSelection
         .style('left', d => {
-          return `${d.x - parseInt(d.width/2)}px`;
+          // console.log(`d width pct: ${d.width / containerRect.width}`)
+          return `${d.x - (d.width / containerRect.width) / 2 * 100}%`;
         }).style('top', d => {
-          return `${d.y - parseInt(d.height/2)}px`
+          // console.log(`d height: ${d.height}`)
+          return `${d.y - (d.height / containerRect.height) / 2 * 100}%`
         });
     });
 
   function drag(simulation) {
     function dragstarted(event) {
-      if (!event.active) simulation.alphaTarget(0.3).restart();
+      if (!event.active) simulation.alphaTarget(0.5).restart();
       event.subject.fx = event.subject.x;
       event.subject.fy = event.subject.y;
     }
@@ -61,6 +65,11 @@ async function homePageGraphAnimation() {
              .on("end", dragended);
   };
   updateSelection.call(drag(simulation));
+  window.onresize = (event) => {
+    if (!event.active) {
+      simulation.alphaTarget(0.3).restart();
+    }
+  }
 }
 
 homePageGraphAnimation();
--- html/nebula.js
+++ html/nebula.js
@@ -1,24 +1,41 @@
 async function pulse_circles() {
+  const svg = document.querySelector('svg');
+
+  const container = document.getElementsByClassName('outline-text-2')[0];
+  container.style.display = 'inline-block';
+  container.style.position = 'relative';
+  container.style.width = '100%';
+  // container.style.paddingBottom = '100%';
+  container.style.verticalAlign = 'top';
+  container.style.overflow = 'hidden';
+
+  container.addEventListener("change", () => {
+    const { width, height } = container.getBoundingClientRect();
+    svg.setAttribute('viewBox', "0 0 " + width + " " + height);
+  });
+
   const ARRAY_LENGTH = 50
-  const POSSIBLE_COLORS = ['#bb00bb', '#bb0000', '#0000bb']
+  const POSSIBLE_COLORS = ['#9bb0ff', '#aabfff', '#cad7ff', '#f8f7ff', '#fff4ea', '#ffd2a1', '#ffcc6f']
+  Array.prototype.sample = function() {
+    return this[Math.floor(Math.random()*this.length)];
+  }
   const circle_data = Array.from(Array(ARRAY_LENGTH)).map(() => ({
-    'x': (Math.random() * 40),
-    'y': (Math.random() * 40),
-    // 'fill': POSSIBLE_COLORS[Math.floor(POSSIBLE_COLORS.length * Math.random())],
-    'fill': '#f4edc6',
+    'x': (Math.random() * 100),
+    'y': (Math.random() * 50),
+    'fill': POSSIBLE_COLORS.sample(),
   }))
   while (true) {
     await d3.select(".circles")
             .selectAll('circle')
             .data(circle_data)
             .join("circle")
-            .attr("r", 0.25)
+            .attr("r", () => Math.random() * 0.25 + 0.125)
             .attr("cx", d => d['x'])
             .attr("cy", d => d['y'])
             .style('fill', d => d['fill'])
             .attr("opacity", 0)
             .transition()
-            .duration(1000)
+            .duration(250)
             .delay((_, i) => i * 250)
             .attr("opacity", 1)
             .end();
