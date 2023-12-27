c5f6acea86d94706b2c08f8787044f49e90d1325
ketanri <ketan@robustintelligence.com>
2023-12-27 01:08:22 -0500

    cleanup

--- html/homepage-graph-animation.js
+++ html/homepage-graph-animation.js
@@ -6,7 +6,7 @@ async function homePageGraphAnimation() {
   }
   // that way the <a>'s are positioned absolutely, relative to the parent
   // https://css-tricks.com/absolute-positioning-inside-relative-positioning/
-  container = d3.select('div[class^=\'outline-text\']')
+  const container = d3.select('div[class^=\'outline-text\']')
                 .style('position', 'relative');
   // select <a> nodes, get their x/y coords
   const selection = d3.selectAll('a:not([href^="http"],[href="/"])')
@@ -14,32 +14,27 @@ async function homePageGraphAnimation() {
   // convert to absolute position
   await selection.transition()
                  .duration(1000)
-                 .style('background-color', 'rgba(107, 114, 128, 0.05)')
+                 .style('background-color', 'rgba(107, 114, 128, 0.20)')
                  .style('border-radius', '5px')
                  .style('padding', '2px')
                  .style('position', 'absolute')
                  .end();
 
   // start a d3 force simulation where the x/y coords are updated on tick
-  const nodes = selection.nodes().map(n => {
-    const boundingRect = n.getBoundingClientRect()
-    return boundingRect
-  });
+  const nodes = selection.nodes().map(n => n.getBoundingClientRect())
   const updateSelection = selection.data(nodes);
+  let containerRect = container.node().getBoundingClientRect();
   const simulation = d3.forceSimulation(nodes)
     .force('x', d3.forceX(50))
     .force('y', d3.forceY(50).strength(0.2))
-    .force('collide', d3.forceCollide(10))
-    .force('charge', d3.forceManyBody().strength(-1))
+    .force('collide', d3.forceCollide().radius(d => d.width / containerRect.width / 2 * 100 + 1))
+    .force('charge', d3.forceManyBody().strength(-20))
     .on('tick', function () {
       containerRect = container.node().getBoundingClientRect();
-      // console.log(`container width: ${containerRect.width} height: ${containerRect.height}`)
       updateSelection
         .style('left', d => {
-          // console.log(`d width pct: ${d.width / containerRect.width}`)
           return `${d.x - (d.width / containerRect.width) / 2 * 100}%`;
         }).style('top', d => {
-          // console.log(`d height: ${d.height}`)
           return `${d.y - (d.height / containerRect.height) / 2 * 100}%`
         });
     });
@@ -47,13 +42,18 @@ async function homePageGraphAnimation() {
   function drag(simulation) {
     function dragstarted(event) {
       if (!event.active) simulation.alphaTarget(0.5).restart();
-      event.subject.fx = event.subject.x;
-      event.subject.fy = event.subject.y;
+      const x = (event.sourceEvent.clientX - containerRect.x) / containerRect.width * 100;
+      const y = (event.sourceEvent.clientY - containerRect.y) / containerRect.height * 100;
+      event.subject.fx = x;
+      event.subject.fy = y;
     }
 
     function dragged(event) {
-      event.subject.fx = event.x;
-      event.subject.fy = event.y;
+      const containerRect = container.node().getBoundingClientRect();
+      const x = (event.sourceEvent.clientX - containerRect.x) / containerRect.width * 100;
+      const y = (event.sourceEvent.clientY - containerRect.y) / containerRect.height * 100;
+      event.subject.fx = x;
+      event.subject.fy = y;
     }
 
     function dragended(event) {
@@ -70,7 +70,8 @@ async function homePageGraphAnimation() {
   updateSelection.call(drag(simulation));
   window.onresize = (event) => {
     if (!event.active) {
-      simulation.alphaTarget(0.3).restart();
+      console.log('resizing');
+      simulation.alphaTarget(0.5).restart();
     }
   }
 }
--- html/nebula.js
+++ html/nebula.js
@@ -6,35 +6,43 @@ async function pulse_circles() {
 
   const svg = document.querySelector('svg');
 
-  const container = document.getElementsByClassName('outline-text-2')[0];
-  container.style.display = 'inline-block';
-  container.style.position = 'relative';
-  container.style.width = '100%';
-  // container.style.paddingBottom = '100%';
-  container.style.verticalAlign = 'top';
-  container.style.overflow = 'hidden';
+  const container = document.getElementsByClassName('svg-container')[0];
 
-  container.addEventListener("change", () => {
-    const { width, height } = container.getBoundingClientRect();
-    svg.setAttribute('viewBox', "0 0 " + width + " " + height);
+  let { width, height } = container.getBoundingClientRect();
+  svg.setAttribute('viewBox', "0 0 " + width + " " + height);
+  svg.setAttribute('width', width);
+  svg.setAttribute('height', height);
+
+  const resizeObserver = new ResizeObserver(entries => {
+    for (let entry of entries) {
+      width = entry.contentRect.width;
+      height = entry.contentRect.height;
+      svg.setAttribute('viewBox', "0 0 " + width + " " + height);
+      svg.setAttribute('width', width);
+      svg.setAttribute('height', height);
+    }
   });
+
+  // Start observing the container
+  resizeObserver.observe(container);
 
   const ARRAY_LENGTH = 50
   const POSSIBLE_COLORS = ['#9bb0ff', '#aabfff', '#cad7ff', '#f8f7ff', '#fff4ea', '#ffd2a1', '#ffcc6f']
   Array.prototype.sample = function() {
     return this[Math.floor(Math.random()*this.length)];
   }
-  const circle_data = Array.from(Array(ARRAY_LENGTH)).map(() => ({
-    x: (Math.random() * 100),
-    y: (Math.random() * 100),
-    fill: POSSIBLE_COLORS.sample(),
-  }))
   while (true) {
+    const circle_data = Array.from(Array(ARRAY_LENGTH)).map(() => ({
+      x: (Math.random() * 100),
+      y: (Math.random() * 100),
+      r: (Math.random() * 0.25 + 0.125),
+      fill: POSSIBLE_COLORS.sample(),
+    }))
     await d3.select(".circles")
             .selectAll('circle')
             .data(circle_data)
             .join("circle")
-            .attr("r", () => `${Math.random() * 0.25 + 0.125}%`)
+            .attr("r", d => `${d.r}%`)
             .attr("cx", d => `${d.x}%`)
             .attr("cy", d => `${d.y}%`)
             .style('fill', d => d.fill)
